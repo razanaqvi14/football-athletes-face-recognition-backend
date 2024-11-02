37f17e23f4db8984cf1ec825007f895289b1894b
razanaqvi <razanaqvi2004@gmail.com>
2024-11-02 17:50:54 +0500

    added port variable in mysql connection

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ cloudinary.config(
 
 db = mysql.connector.connect(
     host=os.getenv("MYSQL_HOST"),
+    port=os.getenv("MYSQL_PORT"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
     database=os.getenv("MYSQL_DB"),
